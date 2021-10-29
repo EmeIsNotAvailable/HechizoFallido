1f51cfc6224fab6a38a0e8d06093f69932f0bdc2
EmeIsNotAvailable <75790968+EmeIsNotAvailable@users.noreply.github.com>
2021-10-29 23:16:34 +0200

    movida

---
+++ Hechizo Fallido/Assets/Scripts/movbrujita.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movbrujita : MonoBehaviour
+{
+    float x;
+    public float vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal") * vel * Time.deltaTime;
+        transform.Translate(x, 0, 0);
+        if (x < 0)
+            transform.localScale = new Vector2(-1, 1);
+        else if(x!=0)
+            transform.localScale = new Vector2(1, 1);
+    }
+}
