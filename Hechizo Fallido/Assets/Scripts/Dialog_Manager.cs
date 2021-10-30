f34695c86913223f941776b6cd44d389c5a128d9
ninyopantano <75835509+ninyopantano@users.noreply.github.com>
2021-10-30 17:10:32 +0200

    cambio2

--- Hechizo Fallido/Assets/Scripts/Dialog_Manager.cs
+++ Hechizo Fallido/Assets/Scripts/Dialog_Manager.cs
@@ -39,12 +39,28 @@ public class Dialog_Manager : MonoBehaviour
             return;
         }
         activeSentence = sentences.Dequeue();
+        displayText.text = activeSentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeTheSentence(activeSentence));
         Debug.Log(activeSentence);
+    }
+    IEnumerator TypeTheSentence(string sentence)
+    {
+        displayText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            displayText.text += letter;
+            myAudio.PlayOneShot(speakSound);
+            yield return new WaitForSeconds(typingSpeed);
+
+
+        }
     }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
-        { 
+        {
+            dialog_panel.SetActive(true);
             startDialogue();
             print("Chocasion");
         }
@@ -53,14 +69,22 @@ public class Dialog_Manager : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if (Input.GetKeyDown("space"))
+            if (Input.GetKeyDown("space")&& displayText.text==activeSentence)
             {
                 DisplayNextSentence();
-                print("poia pero mejor");
             }
             print("poia");
         }
-    }/*
+    }
+    private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            dialog_panel.SetActive(false);
+            StopAllCoroutines();
+        }
+    }
+    /*
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
